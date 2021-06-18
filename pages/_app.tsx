1e21d0aca8d152edc4f5c4b63e23154f7aa44555
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-06-18 12:12:05 -0300

    first changes and added the head & meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,32 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<meta charSet="utf-8" />
+				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+				<meta
+					name="viewport"
+					content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+				/>
+				<meta name="description" content="Tienda virtual Marroquinería Stallion" />
+				<meta name="keywords" content="Marroquineria Stallion" />
+				<title>Marroquinería Stallion</title>
+
+				{/**material UI */}
+				<link
+					rel="stylesheet"
+					href="https://fonts.googleapis.com/icon?family=Material+Icons"
+				/>
+				<link
+					rel="stylesheet"
+					href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	)
 }
 export default MyApp
