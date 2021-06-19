7312e285d7398117e4ecb3fb533744e114107ebf
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-06-19 13:24:25 -0300

    added the map

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,6 +31,20 @@ const useStyles = makeStyles({
 		marginTop: "3rem",
 		fontFamily: "Arial",
 	},
+	contactContainer: {
+		marginTop: "3rem",
+		marginBottom: "3rem",
+		borderRadius: 15,
+		backgroundColor: "#f5f5f5",
+		padding: 50,
+	},
+	map: {
+		border: 0,
+		borderRadius: 15,
+		width: "100%",
+		paddingTop: "3rem",
+		paddingBottom: "3rem",
+	},
 })
 
 export default function Home() {
@@ -53,7 +67,7 @@ export default function Home() {
 				</Grid>
 			</Container>
 			<Container maxWidth="lg" className={classes.galeryContainer}>
-				<Grid container justify="center" spacing={7}>
+				<Grid container justify="center" spacing={4}>
 					<Grid item xs={12} className={classes.imgContainer}>
 						<img
 							src="/images/main_home.jpg"
@@ -68,7 +82,7 @@ export default function Home() {
 						<Divider />
 					</Grid>
 					<Grid item xs={12} sm={6} lg={4}>
-						<Paper elevation={4} className={classes.imgContainer}>
+						<Paper className={classes.imgContainer}>
 							<img
 								src="/images/galery_1.jpg"
 								alt="Tienda online de Marroquinería Stallion"
@@ -77,7 +91,7 @@ export default function Home() {
 						</Paper>
 					</Grid>
 					<Grid item xs={12} sm={6} lg={4}>
-						<Paper elevation={4} className={classes.imgContainer}>
+						<Paper className={classes.imgContainer}>
 							<img
 								src="/images/galery_2.jpg"
 								alt="Tienda online de Marroquinería Stallion"
@@ -86,7 +100,7 @@ export default function Home() {
 						</Paper>
 					</Grid>
 					<Grid item xs={12} sm={6} lg={4}>
-						<Paper elevation={4} className={classes.imgContainer}>
+						<Paper className={classes.imgContainer}>
 							<img
 								src="/images/galery_3.jpg"
 								alt="Tienda online de Marroquinería Stallion"
@@ -95,13 +109,33 @@ export default function Home() {
 						</Paper>
 					</Grid>
 					<Grid item xs={12} sm={6} lg={4}>
-						<Paper elevation={4} className={classes.imgContainer}>
+						<Paper className={classes.imgContainer}>
 							<img
 								src="/images/galery_4.jpg"
 								alt="Tienda online de Marroquinería Stallion"
 								className={classes.image}
 							/>
 						</Paper>
+					</Grid>
+				</Grid>
+			</Container>
+			<Container maxWidth="lg" className={classes.contactContainer}>
+				<Grid container justify="space-around">
+					<Grid item xs={12} className={classes.textCenter} id="galeria">
+						<Typography variant="h3" className={classes.title}>
+							Comunícate con Nosotros
+						</Typography>
+						<Divider />
+					</Grid>
+					<Grid item xs={12}>
+						<iframe
+							src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.7463152327573!2d-58.388261384770125!3d-34.61057588045776!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccadb1b5f88eb%3A0x1fb2acb2510b061e!2sStallion%20Marroquiner%C3%ADa!5e0!3m2!1ses-419!2sar!4v1624119255592!5m2!1ses-419!2sar"
+							width="600"
+							height="450"
+							className={classes.map}
+							allowFullScreen
+							loading="lazy"
+						></iframe>
 					</Grid>
 				</Grid>
 			</Container>
