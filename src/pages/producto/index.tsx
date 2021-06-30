c31283b6d9c2e7261162d434a2d0b3f89defa021
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-06-30 08:44:08 -0300

    created product page

---
+++ src/pages/producto/index.tsx
@@ -0,0 +1,114 @@
+import { Container, Grid, Typography, Paper, Divider } from "@material-ui/core"
+
+import { makeStyles } from "@material-ui/core/styles"
+
+import Image from "next/image"
+
+import ListProductsSection from "../../components/ListProductsSection"
+
+const useStyles = makeStyles({
+	textCenter: {
+		textAlign: "center",
+	},
+	contactContainer: {
+		marginTop: "3rem",
+		marginBottom: "3rem",
+		borderRadius: 15,
+		backgroundColor: "#f5f5f5",
+		padding: 50,
+	},
+	map: {
+		border: 0,
+		borderRadius: 15,
+		width: "100%",
+		paddingTop: "3rem",
+		paddingBottom: "3rem",
+	},
+	contactInfo: {
+		textAlign: "center",
+		marginTop: 50,
+	},
+})
+
+export default function Home() {
+	const classes = useStyles()
+
+	return (
+		<>
+			<ListProductsSection />
+
+			<Container maxWidth="lg" className={classes.contactContainer} id="contacto">
+				<Grid container justify="space-around">
+					<Grid item xs={12} className={classes.textCenter}>
+						<Typography variant="h3">Comunícate con Nosotros</Typography>
+						<Divider />
+					</Grid>
+					<Grid item xs={12}>
+						<iframe
+							src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.7463152327573!2d-58.388261384770125!3d-34.61057588045776!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccadb1b5f88eb%3A0x1fb2acb2510b061e!2sStallion%20Marroquiner%C3%ADa!5e0!3m2!1ses-419!2sar!4v1624119255592!5m2!1ses-419!2sar"
+							width="600"
+							height="450"
+							className={classes.map}
+							allowFullScreen
+							loading="lazy"
+							title="Ecuéntranos en Google Maps"
+						></iframe>
+					</Grid>
+					<Grid item xs={12} md={4} className={classes.contactInfo}>
+						<Typography variant="h6" gutterBottom paragraph>
+							Contacto
+						</Typography>
+						<Typography variant="body1">011 4381-6833</Typography>
+					</Grid>
+					<Grid item xs={12} md={4} className={classes.contactInfo}>
+						<Typography variant="h6" gutterBottom paragraph>
+							Dirección
+						</Typography>
+						<Typography variant="body1">
+							<strong>San José 155</strong> C1076AAC
+						</Typography>
+						<Typography variant="body1">
+							Ciudad Autónoma de Buenos Aires, Argentina
+						</Typography>
+					</Grid>
+					<Grid item xs={12} md={4} className={classes.contactInfo}>
+						<Typography variant="h6" gutterBottom paragraph>
+							Horarios de atención
+						</Typography>
+						<Grid container justify="center" style={{ textAlign: "left" }}>
+							<Grid item xs={12} md={6}>
+								<Grid container spacing={1}>
+									<Grid item xs={12}>
+										<Typography variant="body1">
+											Lunes: 8:30 - 18:00 hs
+										</Typography>
+									</Grid>
+									<Grid item xs={12}>
+										<Typography variant="body1">
+											Martes: 8:30 - 18:00 hs
+										</Typography>
+									</Grid>
+									<Grid item xs={12}>
+										<Typography variant="body1">
+											Miércoles: 8:30 - 18:00 hs
+										</Typography>
+									</Grid>
+									<Grid item xs={12}>
+										<Typography variant="body1">
+											Jueves: 8:30 - 18:00 hs
+										</Typography>
+									</Grid>
+									<Grid item xs={12}>
+										<Typography variant="body1">
+											Viernes: 8:30 - 18:00 hs
+										</Typography>
+									</Grid>
+								</Grid>
+							</Grid>
+						</Grid>
+					</Grid>
+				</Grid>
+			</Container>
+		</>
+	)
+}
