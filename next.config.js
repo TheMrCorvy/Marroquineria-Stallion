be8dcd3c9240746bda24cd9322ca0da068a08ff6
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-07-27 11:32:47 -0300

    uploading semi-final version to test payments

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	reactStrictMode: true,
 	images: {
-		domains: ["st4.depositphotos.com"],
+		domains: ["st4.depositphotos.com", "stallion-marroquineria.s3.sa-east-1.amazonaws.com"],
 	},
 }
