97086c0fe0922bb0f4dd1c879386fed9f1a35a55
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-08-19 11:30:52 -0300

    finished styiling

--- src/components/MegaMenu.tsx
+++ src/components/MegaMenu.tsx
@@ -22,58 +22,54 @@ const MegaMenu: FC<Props> = ({ className }) => {
         onMouseLeave={() => setIsShown(false)}
       >
         <div className="header">
-          <h2>Todas las Categorías Disponibles</h2>
+          <h2 style={{ marginBottom: 0 }}>Todas las Categorías Disponibles</h2>
         </div>
-        <div className="row">
+        <div className="row" style={{ paddingLeft: "4rem" }}>
           <div className="column">
+            <h2>Accesorios</h2>
             <a href="#">Accesorios</a>
-            <a href="#">Accesorios de Mujer</a>
+            <a href="#">Accesorios Mujer</a>
             <a href="#">Accesorios de Viaje</a>
-            <a href="#">Bandoleras</a>
+            <a href="#">Barbijos / Cubrebocas</a>
+            <a href="#">Paraguas</a>
+            <a href="#">Paraguas de Hombre y de Mujer</a>
+            <a href="#">Porta Cosméticos</a>
           </div>
           <div className="column">
-            <a href="#">Barbijos / Cubrebocas</a>
+            <h2>Morrales</h2>
+            <a href="#">Bandoleras</a>
+            <a href="#">Bolsos</a>
+            <a href="#">Carteras</a>
+            <a href="#">Carteras de Dama</a>
+            <a href="#">Carteras de Dama (Simil Cuero)</a>
+            <a href="#">Morrales</a>
+            <a href="#">Riñoneras</a>
+          </div>
+          <div className="column">
+            <h2>Billeteras</h2>
             <a href="#">Billeteras</a>
             <a href="#">Billeteras - Hombre</a>
             <a href="#">Billeteras - Mujer</a>
-          </div>
-          <div className="column">
-            <a href="#">Billeteras - Tarjetero</a>
+            <a href="#">Billeteras Tarjetero</a>
             <a href="#">Billeteras de Cuero - Hombre</a>
             <a href="#">Billeteras de Cuero - Mujer</a>
-            <a href="#">Bolsos</a>
           </div>
           <div className="column">
+            <h2>Maletines</h2>
             <a href="#">Botineros</a>
-            <a href="#">Carteras</a>
-            <a href="#">Carteras de Dama</a>
-            <a href="#">Carteras (Simil Cuero)</a>
+            <a href="#">Maletines</a>
+            <a href="#">Porta Notebooks</a>
+            <a href="#">Portafolios</a>
+            <a href="#">Portafolios para Notebooks</a>
+            <a href="#">Portafolios (Simil Cuero)</a>
           </div>
           <div className="column">
+            <h2>Mochilas</h2>
             <a href="#">Cartucheras</a>
-            <a href="#">Maletines</a>
             <a href="#">Mochilas</a>
             <a href="#">Mochilas Escolares</a>
-          </div>
-          <div className="column">
             <a href="#">Mochilas Porta Notebooks</a>
-            <a href="#">Mochila Urbanas</a>
-            <a href="#">Morrales</a>
-            <a href="#">Paraguas</a>
-          </div>
-          <div className="column">
-            <a href="#">Paraguas de Hombre y de Mujer</a>
-            <a href="#">Porta Cosméticos</a>
-            <a href="#">Porta Notebooks</a>
-          </div>
-          <div className="column">
-            <a href="#">Portafolios</a>
-            <a href="#">Portafolios Porta Notebooks</a>
-            <a href="#">Portafolios (Simil Cuero)</a>
-            <a href="#">Productos Fabricados</a>
-          </div>
-          <div className="column">
-            <a href="#">Riñoneras</a>
+            <a href="#">Mochilas Urbanas</a>
             <a href="#">Valijas</a>
           </div>
         </div>
